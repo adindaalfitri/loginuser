e54405a86bb1944cc93ce550fe570281ffc64e04
adindaalfitri <99473728+adindaalfitri@users.noreply.github.com>
2022-09-16 09:19:46 +0700

    .

---
+++ datasiswa.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Siswa (siswa)</title>
+</head>
+<body>
+    <h1>Data Siswa (siswa)</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Foto</th>
+            <th>Nama</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td><img src="adinda.jpeg" width="200px"></td>
+            <td>Adinda</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td><img src="fitri.jpeg" width="200px"></td>
+            <td>Fitri</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td><img src="hutomo.jpeg" width="200px"></td>
+            <td>Hutomo</td>
+        </tr>
+    </table>
+        <br>
+        <a href="login.php"><button>Back</button></a>
+</body>
+</html>
